0ff423cd3d63a43b4052c7367776ec543c837aae
yaellerech <48100558+yaellerech@users.noreply.github.com>
2019-05-28 12:43:21 +0300

    add SetMove function

--- Game.c
+++ Game.c
@@ -1,1 +1,6 @@
 
+
+void SetMove (int column, int row, int value){
+
+}
+
--- Game.h
+++ Game.h
@@ -1,1 +1,9 @@
+#ifndef GAME_H_
+#define GAME_H_
+
+void SetMove (int column, int row, int value)
+
+
+
+#endif /* GAME_H_ */
 
