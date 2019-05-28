98aa99e630fb1bc5d46918bb0a0d96d74e1d3b88
yaellerech <48100558+yaellerech@users.noreply.github.com>
2019-05-28 15:20:00 +0300

    test

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 
 #include <stdio.h>
 #define NumOfCells 80
+
+
 int main(){
 	printf("Please enter the number of cells to fill [0-80]:\n");
 	if (!checkNumOfCellsToFill(NumOfCells)){  /* function to main_aux */
