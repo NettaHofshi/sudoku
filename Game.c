69584376ced47584978446b72002b80a3331acef
Netta Hofshi <nettaho@gmail.com>
2019-05-29 12:02:02 +0300

    added to Game.c and Game.h

--- Game.c
+++ Game.c
@@ -37,7 +37,7 @@ void printBoard(char board[][][]){
 	printCells(board, 6);
 }
 
-int columnValid(int column, int value, int board[][][]){
+int columnValid(int column, int value, char board[][][]){
 	for(int i=0; i<9; i++){
 		if(board[i][column][1] == value){
 			return 0;
@@ -46,7 +46,7 @@ int columnValid(int column, int value, int board[][][]){
 	return 1;
 }
 
-int rowValid(int row, int value, int board[][][]){
+int rowValid(int row, int value, char board[][][]){
 	for(int i=0; i<9; i++){
 		if(board[row][i][1] == value){
 			return 0;
@@ -55,23 +55,49 @@ int rowValid(int row, int value, int board[][][]){
 	return 1;
 }
 
-int squareValid(int column, int row, int value, int board[][][]){
+int squareValid(int column, int row, int value, char board[][][]){
 	/*need to implement*/
 	return 1;
 }
 
-void SetMove (int column, int row, int value, int board[][][]){
+int gameOver(char board[][][]){
+	for(int i=0; i<9; i++){
+		for(int j=0; j<9; j++){
+			if(board[i][j][1] == null){
+				return 0;
+			}
+		}
+	}
+	return 1;
+}
+
+void SetMove (int column, int row, int value, char board[][][]){
 	if(board[row-1][column-1][0] == "."){
 		printf("Error: cell is fixed\n");
 	}
-	if(columnValid(column-1, value, board)==0){
+	else if(columnValid(column-1, value, board)==0){
 		printf("Error: value is invalid\n");
 	}
-	if(rowValid(row-1, value, board)==0){
+	/* call set again? */
+	else if(rowValid(row-1, value, board)==0){
 		printf("Error: value is invalid\n");
 	}
-	if(squareValid(column-1, row-1, value, board)==0){
+	else if(squareValid(column-1, row-1, value, board)==0){
 		printf("Error: value is invalid\n");
+	}
+	else if(value!=0){
+		board[row-1][column-1][1]=value;
+	}
+	else{
+		board[row-1][column-1][1] = null;
+	}
+	if(gameOver(board) == 1){
+		printf("Puzzle solved successfully\n");
 	}
 }
+
+char hint(int column, int row, char board[][][]){
+	return board[row-1][column-1][1];
+}
+
 
--- Game.h
+++ Game.h
@@ -1,9 +1,15 @@
 #ifndef GAME_H_
 #define GAME_H_
 
-void SetMove (int column, int row, int value);
-void printBoard(int board[][]);
-
+void SetMove (int column, int row, int value, char board[][][]);
+void printBoard(char board[][][]);
+char hint(int column, int row, char board[][][]);
+int gameOver(char board[][][]);
+int squareValid(int column, int row, int value, char board[][][]);
+int rowValid(int row, int value, char board[][][]);
+int columnValid(int column, int value, char board[][][]);
+void printCells(char board[][][], int index);
+void printDashes();
 
 
 #endif /* GAME_H_ */
