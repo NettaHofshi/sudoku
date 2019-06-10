ba4e49b67a89a394a73dc77d87499e1d196ec246
yaellerech <48100558+yaellerech@users.noreply.github.com>
2019-06-10 19:26:16 +0300

    nettaaaaaaa

--- Game.h
+++ Game.h
@@ -1,42 +1,66 @@
 #ifndef GAME_H_
 #define GAME_H_
 
-/*
+/* setMove function:
  * updates the value in the required cell in the board if it is valid
  * if the game is over, updates the user
+ *
+ * parameters-
+ * column- the column of the cell to insert in
+ * row-the column of the cell to insert in
+ * value-the value to insert
+ * board- the gameboard that the move will be played at
  */
 void setMove (int column, int row, int value, char board[][9][2]);
 
-/*
+/* printBoard function:
  * prints the board of the game
+ *
+ * parameters-
+ * board- the board we want to print
  */
 void printBoard(char board[][9][2]);
 
-/*
+/* hint function:
  * gives the user the desired hint
  * the hint is the value located in the required cell in the solved board
+ *
+ * parameters-
+ * column- the the column of the cell to recieve
+ * row- the the row of the cell to recieve
+ * board- the solved board to take from
  */
 char hint(int column, int row, char board[][9][2]);
 
-/*
+/* gameOver function:
  * checks if the board of the game is full
  * returns 1 if the board is full, and 0 else
+ *
+ * parameters-
+ * board- the game board
  */
 int gameOver(char board[][9][2]);
 
-/*
+/* exitGame function:
  * terminates the game
  */
 void exitGame();
 
-/*
+/* restart function:
  * restarts the game
+ *
+ * parameters-
+ * restartValue- pointer to a int that tells you if to reatart the game or not
  */
-void restart(int* restar);
+void restart(int* restartValue);
 
-/*
+/* validate function:
  * checks if the board of the game is solvable
  * prints to the user if the game is solvable or not
+ *
+ * parameters-
+ * gameBoard- the board we need to check if is valid
+ * solvedBoard- the board we need to update to the current solution
  */
 void validate(char gameBoard[][9][2],char solvedBoard[][9][2]);
 
--- MainAux.h
+++ MainAux.h
@@ -3,13 +3,24 @@
 #define MAINAUX_H_
 
 /*
- * function- checkNumOfCellsToFill
+ * checkNumOfCellsToFill function:
  * checks if the the number the user entred is legal (in the right range)
  *
- * int range= the range it needs to be in
+ * parameters
+ * range- the range it needs to be in
  *
  */
 int checkNumOfCellsToFill (int range);
+
+/*
+ * initAllBoards function:
+ * initialize all the board that will be used during the game, to '0' and ' '
+ *
+ * parameters-
+ * gameBoard
+ * solvedBoard
+ * tempBoard
+ */
 void initAllBoards(char gameBoard [][9][2],char solvedBoard [][9][2],char tempBoard[9][9][10]);
 
 
